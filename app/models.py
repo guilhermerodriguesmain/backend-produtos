d1fba799dcd6fafb2a920980d49c89186126505c
Guilherme <guilhermerodrigues9369@gmail.com>
2026-06-16 15:00:48 -0300

    feat(models): implementa modelos das tabelas do banco de dados

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, Float, Integer, String
+
+from app.database import Base
+
+
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    preco = Column(Float, nullable=False)
+    estoque = Column(Integer, default=0)
+    ativo = Column(Boolean, default=True)
+    
