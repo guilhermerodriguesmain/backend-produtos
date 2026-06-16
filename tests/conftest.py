f022373cc9aa948e13aabcb05b3421f9e3713cdb
Guilherme <guilhermerodrigues9369@gmail.com>
2026-06-16 18:57:08 -0300

    feat(tests): implementa conftest.py com recursos para tests

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.database import Base, get_db
+from app.main import app
+
+load_dotenv()
+
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+
+engine = create_engine(TEST_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client():
+    Base.metadata.create_all(bind=engine)
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
+
+    Base.metadata.drop_all(bind=engine)
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def produto_existente(client):
+    response = client.post(
+        "/produtos",
+        json={
+            "nome": "Mouse Gamer",
+            "preco": 199.90,
+            "estoque": 10,
+            "ativo": True
+        }
+    )
+
+    return response.json()
